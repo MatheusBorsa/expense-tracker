c5af05ac79b6d7482b8b2fb443abd19e019d70d7
MatheusBorsa <matheusalek485@icloud.com>
2024-10-28 16:24:08 -0700

    Initial Commit

---
+++ Models/ExpenseTrackerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace expensetracker.Models
+{
+    public class ExpenseTrackerDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public ExpenseTrackerDbContext(DbContextOptions<ExpenseTrackerDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
